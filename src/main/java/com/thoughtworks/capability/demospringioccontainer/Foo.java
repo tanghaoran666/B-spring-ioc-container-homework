7ea336fb66da799391b4e05648e4d37f0bbd806a
tanghaoran666 <18408230362@163.com>
2020-11-07 21:11:09 +0800

    q3 method1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,15 +1,12 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Foo {
-
-    private Bar bar;
-
-    public Foo(Bar bar) {
-        this.bar = bar;
-    }
+    @Autowired
+    Bar bar;
 
     public void hi() {
         System.out.println("Hi, " + bar.name());
