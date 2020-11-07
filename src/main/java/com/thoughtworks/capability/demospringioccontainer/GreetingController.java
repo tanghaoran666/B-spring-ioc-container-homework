33e818a2e63040d7c1e6a823bcc2ce2c9e02dbc3
tanghaoran666 <18408230362@163.com>
2020-11-07 20:52:45 +0800

    q2 method1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,17 +1,18 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Scope("singleton")
 @RestController
 public class GreetingController {
 
     private final GreetingService greetingService;
 
-    @Autowired
     public GreetingController(GreetingService greetingService) {
         this.greetingService = greetingService;
+        System.out.println("new controller");
     }
 
     @GetMapping("/greet")
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
+@Scope(value="prototype", proxyMode= ScopedProxyMode.TARGET_CLASS)
 @Service
 public class GreetingService {
 
+    GreetingService() {
+        System.out.println("new service");
+    }
     String sayHi() {
         return "hello world";
     }
